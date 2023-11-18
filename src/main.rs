096c1ce1709482088a1c8126e92468b8f2cc12ec
Mike Young <mikeyoung3k@gmail.com>
2023-11-18 20:51:11 +0000

    A more complex situation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::collections::HashMap;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut inventory = HashMap::new();
+    inventory.insert("servers".to_string(),5);
+    inventory.insert("screens".to_string(),3);
+
+    let customer = Session{
+        customer: "Foo".to_string(),
+        cart: vec!["Pliers".to_string(),"Rice".to_string(),"Toothpaste".to_string()],
+        inventory: inventory,
+        account_type: Account::Corporate("CompuTech".to_string()),
+        shipping: Address{
+            country: "Austria".to_string(),
+            location: ["Mouse".to_string(), "Coffee".to_string(), "Tall".to_string()],
+            postcode: "HA34 7ER".to_string(),
+        }
+    };
+
+    println!("{:?}", customer);
+}
+
+#[derive(Debug)]
+struct Session {
+    customer: String,
+    cart: Vec<String>,
+    inventory: HashMap<String, usize>,
+    account_type: Account,
+    shipping: Address,
+}
+
+#[derive(Debug)]
+enum Account {
+    Free,
+    Premium,
+    Corporate(String),
+}
+
+#[derive(Debug)]
+struct Address {
+    country: String,
+    location: [String;3],
+    postcode: String,
 }
