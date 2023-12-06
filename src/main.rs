f7181e5f90bc0dd167ebd1befd77bbd73649ef42
Mike Young <mikeyoung3k@gmail.com>
2023-12-06 18:27:14 +0000

    A configurable log level

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+#![allow(dead_code)]
+
 use std::collections::HashMap;
 use serde::Serialize;
+use std::fmt;
 
+use log::{debug,info,error};
+use env_logger::Env;
 
 fn main() {
+    let env = Env::default().filter_or("LOG_LEVEL","info");
+    env_logger::init_from_env(env);
+
     let mut inventory = HashMap::new();
     inventory.insert("servers".to_string(),5);
     inventory.insert("screens".to_string(),3);
@@ -18,8 +26,12 @@ fn main() {
             postcode: "HA34 7ER".to_string(),
         }
     };
-    let log = serde_json::to_string(&customer).unwrap();
-    println!("{}", log)
+
+    info!("{}",customer);
+    debug!("A Debug log: {}",customer);
+    error!("An error log: {}",customer);
+
+
 }
 
 #[derive(Serialize)]
@@ -29,6 +41,16 @@ struct Session {
     inventory: HashMap<String, usize>,
     account_type: Account,
     shipping: Address,
+}
+
+impl fmt::Display for Session {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let log = serde_json::to_string(&self);
+        match log {
+            Ok(l) => write!(f,"{}",l),
+            Err(e) => write!(f,"Failed to serialize log struct: {}",e),
+        }
+    }
 }
 
 #[derive(Serialize)]
